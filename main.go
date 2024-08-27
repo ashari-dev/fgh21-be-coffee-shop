96fd312f9e395f76781edcbd7ded4c197e80f5e2
ashari-dev <boykid923@gmail.com>
2024-08-27 19:36:06 +0700

    feat: setup MVC

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	r.Use(cors.New(config))
+
+	r.Run("0.0.0.0:8000")
 
 }
